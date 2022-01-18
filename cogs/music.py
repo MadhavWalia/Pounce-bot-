2c77c216184d15029def23af5110a78b21fe7475
grc-04 <91790502+grc-04@users.noreply.github.com>
2022-01-19 01:10:03 +0530

    Changed 1 line
    Changed 1 line

--- cogs/music.py
+++ cogs/music.py
@@ -57,7 +57,7 @@ class Music(commands.Cog):
 
             self.music_queue.pop(0)
 
-            self.vc.play(discord.FFmpegPCMAudio(music_url, **self.FFMPEG_OPTIONS), after = lambda e: self.play_next())
+            self.vc.play(discord.FFmpegPCMAudio( music_url, **self.FFMPEG_OPTIONS), executable="C:\FFmpeg\bin\ffmpeg.exe",after = lambda e: self.play_next())
         else:
             self.isPlaying = False
 
