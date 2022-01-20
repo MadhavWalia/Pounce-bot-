f584339bd4db6665dc3ce041ac7c58e8cd065962
Madhav_Walia <73129716+MadhavWalia@users.noreply.github.com>
2022-01-20 21:12:43 +0530

    Pounce command

--- cogs/scoresheet.py
+++ cogs/scoresheet.py
@@ -1,14 +1,15 @@
-import enum
-from tkinter.messagebox import QUESTION
 import discord
 from discord.ext import commands
+import asyncio
+
 import os
 import pandas as pd
+
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-
 curr = os.getcwd()
+TEAM_MAX = 12
 
 class Scoresheet(commands.Cog):
 
@@ -16,6 +17,54 @@ class Scoresheet(commands.Cog):
         self.client = client
         self.ques = 0
         self.team_no = 0
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Bot is Online')
+
+    async def sendMessage(self, ctx, message: str):
+        for i in range(TEAM_MAX+1):
+            channel = discord.utils.get(ctx.guild.channels, name=f"team-{i}")
+            if channel is not None:
+                channel_id = channel.id
+                channel = self.client.get_channel(channel_id)
+                await channel.send(message)
+
+
+    @commands.command(name="start") 
+    async def count(self,ctx, number :int = None):
+        try:
+            #If Number isn't assigned
+            if number is None:
+                number = 15
+
+            if number < 0:
+                await ctx.send('How does a negative timer even work!')
+
+            elif number > 300:
+                await ctx.send('Bruh just count upto that urself, not my job')
+
+            else:
+                message = await ctx.send(f"Pounce window closes in `{str(number)}` seconds")
+
+                #Sending Window Open
+                msg = f"Pounce window closes in `{str(number)}` seconds"
+                await self.sendMessage(ctx,msg)
+
+                #Countdown
+                while number != 0:
+                    number -= 1
+                    await message.edit(content= f"Pounce window closes in `{str(number)}` seconds")
+                    await asyncio.sleep(1)
+
+                #Sending window closed
+                msg = "Pounce window closed!"
+                await message.edit(content=f":spy: Pounce window closed!  ",delete_after = 5)
+                await self.sendMessage(ctx, msg)
+
+        except ValueError:
+            await ctx.send('Time was not a number')
+
 
     #Authorization
     def authorize(self):
@@ -67,15 +116,10 @@ class Scoresheet(commands.Cog):
         worksheet.update_cells(cell_list, value_input_option='USER_ENTERED')
 
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print('Bot is Online')
-
-
     @commands.command(name="create")
     async def createSheet(self, ctx, questions: int, team_number: int, email: str):
 
-        if questions>0 and team_number>0:
+        if questions>0 and team_number>0 and team_number<=TEAM_MAX:
 
             #Authorization
             gc = self.authorize()
@@ -96,6 +140,8 @@ class Scoresheet(commands.Cog):
             #Sending Confirmation
             await ctx.send('Scoresheet has been created and sent, as if anyone is going to attend ur mubai style quiz lmao')                   
         
+        elif team_number>TEAM_MAX:
+            await ctx.send('No More than 12 teams')
         else:
             await ctx.send('Team and Question Numbers cannot be negative you potato brained human')
 
@@ -107,7 +153,7 @@ class Scoresheet(commands.Cog):
 
         #Opening SpreadSheet
         try:
-            Scoresheet = gc.open('Score Sheet')
+            ScoreSheet = gc.open('Score Sheet')
         except gspread.exceptions.SpreadsheetNotFound:
             await ctx.send("The ScoreSheet does not exist")
 
@@ -118,9 +164,9 @@ class Scoresheet(commands.Cog):
         )
 
         try:
-            scores = Scoresheet.worksheet("Scores")
+            scores = ScoreSheet.worksheet("Scores")
         except gspread.exceptions.WorksheetNotFound:
-            await ctx.send("Scores sheets is unavailable")
+            await ctx.send("Scores sheet is unavailable")
 
         total = scores.col_values(self.ques + 2)[1:]
         table = [[f"Team{i+1}", j] for i,j in enumerate(total)]
@@ -130,6 +176,35 @@ class Scoresheet(commands.Cog):
 
         await ctx.send(embed=embed)
 
+
+    @commands.command(name="pounce")
+    async def pounce(self, ctx, question : int, answer : str):
+        #Getting Team Number
+        roles = ctx.author.roles
+        role = [name.name for name in roles if name.name[:5]=='team-']
+        await ctx.send("Your Pounce has been noted, get ready for the negs lmao")
+
+        #Adding Pounce to Sheet
+        team_number = int(role[0][-1])
+
+        #Authorization
+        gc = self.authorize()
+
+        #Opening SpreadSheet
+        try:
+            ScoreSheet = gc.open('Score Sheet')
+        except gspread.exceptions.SpreadsheetNotFound:
+            await ctx.send("The ScoreSheet does not exist")
+
+        #Opening Pounce Sheet
+        try:
+            pounces = ScoreSheet.worksheet("Pounces")
+        except gspread.exceptions.WorksheetNotFound:
+            await ctx.send("Pounces sheet is unavailable")
+
+        #Entering Pounce in the Sheet
+        pounces.update_cell(team_number + 1, question + 1, answer)
+        
 
 def setup(client):
     client.add_cog(Scoresheet(client))
